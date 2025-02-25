c7cf211bb1b98e134ddcd43fba5f0d48b57d833f
Losen2133 <carreonromer@gmail.com>
2025-02-25 17:29:19 +0800

    Added functionality
    - Edit and Delete action button added functionality
    - Added @capacitor/preferences for states

--- src/app/pages/file-list-page/file-list-page.page.ts
+++ src/app/pages/file-list-page/file-list-page.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Router } from '@angular/router';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-file-list-page',
@@ -10,9 +12,13 @@ import { Filesystem, Directory } from '@capacitor/filesystem';
 export class FileListPagePage implements OnInit {
   fileList: { name: string; content: string | any;}[] = [];
   pageTitle: string = "Files";
-  constructor() { }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
     this.loadFiles();
   }
 
@@ -22,10 +28,10 @@ export class FileListPagePage implements OnInit {
         path: "",
         directory: Directory.Documents
       });
-
+  
       this.fileList = [];
-
-      for(const file of result.files) {
+  
+      for (const file of result.files) {
         try {
           const fileData = await Filesystem.readFile({
             path: file.name,
@@ -34,11 +40,42 @@ export class FileListPagePage implements OnInit {
   
           this.fileList.push({ name: file.name, content: fileData.data });
         } catch (error) {
-          console.log("Error reading file", error)
+          console.log("Error reading file", error);
         }
       }
     } catch (error) {
       console.log("Error reading directory", error);
     }
+  }
+  
+
+  async deleteFile(file: {name: string, content: string}) {
+    try{
+      await Filesystem.deleteFile({
+        path: file.name,
+        directory: Directory.Documents
+      });
+      alert("Delete successful");
+      this.loadFiles();
+    } catch (error) {
+      console.log("Error deleting file", error);
+    }
+  }
+
+  async newFile() {
+    await Preferences.set({key: 'state', value: 'new'});
+    this.router.navigate(['home/main-page']);
+  }
+
+  async editFile(file: {name: string, content: string}) {
+    await Preferences.set({key: 'state', value: 'edit'});
+
+    const fileData = {
+      fileName: file.name,
+      fileContent: file.content
+    }
+
+    await Preferences.set({key: 'fileData', value: JSON.stringify(fileData)});
+    this.router.navigate(['home/main-page']);
   }
 }
